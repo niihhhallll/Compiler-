facdc0ed6ae24afa8786dd71b3922064eec88a27
M <nihal.linux@proton.me>
2026-09-16 21:52:28 +0530

    Refactored Code For reader.cpp

--- build/CMakeFiles/Com.dir/src/main.cpp.o.d
+++ build/CMakeFiles/Com.dir/src/main.cpp.o.d
@@ -159,21 +159,26 @@ CMakeFiles/Com.dir/src/main.cpp.o: \
  /usr/include/c++/13/bits/stl_bvector.h \
  /usr/include/c++/13/bits/vector.tcc /usr/include/c++/13/optional \
  /usr/include/c++/13/bits/enable_special_members.h \
- /usr/include/c++/13/memory /usr/include/c++/13/bits/stl_tempbuf.h \
- /usr/include/c++/13/bits/stl_raw_storage_iter.h \
- /usr/include/c++/13/bits/align.h \
+ /usr/include/c++/13/filesystem /usr/include/c++/13/bits/fs_fwd.h \
+ /usr/include/c++/13/cstdint \
  /usr/lib/gcc/x86_64-linux-gnu/13/include/stdint.h /usr/include/stdint.h \
  /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
  /usr/include/x86_64-linux-gnu/bits/stdint-least.h \
- /usr/include/c++/13/bits/unique_ptr.h \
+ /usr/include/c++/13/bits/chrono.h /usr/include/c++/13/ratio \
+ /usr/include/c++/13/limits /usr/include/c++/13/ctime \
+ /usr/include/c++/13/bits/parse_numbers.h \
+ /usr/include/c++/13/bits/fs_path.h /usr/include/c++/13/locale \
+ /usr/include/c++/13/bits/locale_facets_nonio.h \
+ /usr/include/x86_64-linux-gnu/c++/13/bits/time_members.h \
+ /usr/include/x86_64-linux-gnu/c++/13/bits/messages_members.h \
+ /usr/include/libintl.h /usr/include/c++/13/bits/locale_facets_nonio.tcc \
+ /usr/include/c++/13/bits/locale_conv.h /usr/include/c++/13/iomanip \
+ /usr/include/c++/13/bits/quoted_string.h /usr/include/c++/13/sstream \
+ /usr/include/c++/13/bits/sstream.tcc /usr/include/c++/13/codecvt \
+ /usr/include/c++/13/ext/concurrence.h \
  /usr/include/c++/13/bits/shared_ptr.h \
  /usr/include/c++/13/bits/shared_ptr_base.h \
  /usr/include/c++/13/bits/allocated_ptr.h \
+ /usr/include/c++/13/bits/unique_ptr.h \
  /usr/include/c++/13/ext/aligned_buffer.h \
- /usr/include/c++/13/ext/concurrence.h \
- /usr/include/c++/13/bits/shared_ptr_atomic.h \
- /usr/include/c++/13/bits/atomic_base.h \
- /usr/include/c++/13/bits/atomic_lockfree_defines.h \
- /usr/include/c++/13/backward/auto_ptr.h \
- /usr/include/c++/13/pstl/glue_memory_defs.h \
- /usr/include/c++/13/pstl/execution_defs.h
+ /usr/include/c++/13/bits/fs_dir.h /usr/include/c++/13/bits/fs_ops.h
--- src/reader.cpp
+++ src/reader.cpp
@@ -3,15 +3,14 @@
 #include <vector>
 #include <iostream>
 #include <optional>
-#include <memory>
+#include <filesystem>
 #include "../include/reader.hpp"
 
-    std::optional<std::vector<std::string>> Reader::Read(const std::string& FilePath)
+    std::optional<std::vector<std::string>> Reader::Read(const std::filesystem::path& FilePath)
     {
-        // exception handling
 
         // constructor for opening file.
-        std::fstream File(FilePath);
+        std::ifstream File(FilePath);
         /*
          * Checks if the file is opened or not
          * else it Prints a error message and throws a nullopt;
@@ -22,20 +21,12 @@
             return std::nullopt;
         }
 
-        // makes a heap allocated smart pointer for VecLine.
-        // this is only for the readability, i will make this into a `auto VecLine`.
         std::vector<std::string> VecLine{};
-        /*
-         * Temp Line object
-         * it reads line by line.
-         * and then feeds to VecLine
-         * so it can return to the function called.
-         */
         std::string Line;
         while(std::getline(File,Line))
         {
             // push back to the vector.
-            VecLine->push_back(Line);
+            VecLine.push_back(std::move(Line));
         }
         return VecLine;
         }
