c18a7bafc9a80d1d60c39cf4c473bd887b287874
M <nihal.linux@proton.me>
2026-09-22 22:48:33 +0530

    reader.cpp changed.

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -19,35 +19,4 @@ struct Token
 
 namespace Lexer
 {
-
-    // if return 0;
-    // refactor it later.
-    Token Check(const std::string& Buffer)
-    {
-        if(Buffer == "return")
-        {
-            return Token{KeyWord,"return",1};
-        }
-        else {
-            return Token{KeyWord,"Unknow",0};
-        }
-    }
-    std::string buffer;
-    std::vector<Token> data{};
-    std::optional<std::vector<Token>> Lexer(std::string& Line)
-    {
-        for(char a : Line)
-        {
-            if(std::isalpha(static_cast<unsigned char>(a)))
-            {
-                buffer.push_back(a);
-            }
-
-            else if (std::isspace(static_cast<unsigned char>(a)))
-            {
-                data.push_back(Check(buffer));
-                continue;
-            }
-        }
-    }
 }
